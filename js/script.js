df497964aa39b8012cbb763f8c2739a7888c7e1b
vladimir-ascue <71859596+vladimir-ascue@users.noreply.github.com>
2023-02-08 11:07:19 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ document.getElementbyId("dedo")
 Tu Identidad
 Lo primero que deberás hacer cuando instales Git es establecer tu nombre de usuario y dirección de correo electrónico. Esto es importante porque los "commits" de Git usan esta información, y es introducida de manera inmutable en los commits que envías:
 
+
+/*******************************************/
+/*******************************************/
+/*******************************************/
+/*******************************************/
+/*******************************************/
+
+
 $ git config --global user.name "John Doe"
 $ git config --global user.email johndoe@example.com
 De nuevo, sólo necesitas hacer esto una vez si especificas la opción --global, ya que Git siempre usará esta información para todo lo que hagas en ese sistema. Si quieres sobrescribir esta información con otro nombre o dirección de correo para proyectos específicos, puedes ejecutar el comando sin la opción --global cuando estés en ese proyecto.
